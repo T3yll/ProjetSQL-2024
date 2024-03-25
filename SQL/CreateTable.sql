2c29172df86f31072ca229c92034d89b71a82909
Nathan Bertaud <nathan.bertaud@ynov.com>
2024-03-25 13:21:40 +0100

    [FIX] database create tables

--- SQL/CreateTable.sql
+++ SQL/CreateTable.sql
@@ -1,76 +1,71 @@
 -- table Adresse
-CREATE TABLE Adresse (
-  AdresseId INT PRIMARY KEY,
-  Numero INT NOT NULL,
-  Rue VARCHAR(255) NOT NULL,
-  Ville VARCHAR(255) NOT NULL,
-  CodePostal INT NOT NULL,
-  Pays VARCHAR(255) NOT NULL,
-);
+CREATE TABLE IF NOT EXISTS Adresse (
+            AdresseId INT PRIMARY KEY AUTO_INCREMENT,
+            Numero INT NOT NULL,
+            Rue VARCHAR(255) NOT NULL,
+            Ville VARCHAR(255) NOT NULL,
+            CodePostal INT NOT NULL,
+            Pays VARCHAR(255) NOT NULL);
 
 -- table Restaurant
-CREATE TABLE Restaurant (
-    RestaurantId INT PRIMARY KEY,
-    Nom VARCHAR(255) NOT NULL,
-    Ville VARCHAR(255) NOT NULL,
-    AdresseId INT NOT NULL,
-    FOREIGN KEY (AdresseId) REFERENCES Adresse(AdresseId),
-);
+CREATE TABLE IF NOT EXISTS  Restaurant (
+            RestaurantId INT PRIMARY KEY AUTO_INCREMENT,
+            Nom VARCHAR(255) NOT NULL,
+            Ville VARCHAR(255) NOT NULL,
+            AdresseId INT NOT NULL,
+            FOREIGN KEY (AdresseId) REFERENCES Adresse(AdresseId)
+        );
 
 -- table Client
-CREATE TABLE Client (
-  ClientId INT PRIMARY KEY,
-  Nom VARCHAR(255) NOT NULL,
-  Prenom VARCHAR(255) NOT NULL,
-);
+CREATE TABLE IF NOT EXISTS  Client (
+            ClientId INT PRIMARY KEY AUTO_INCREMENT,
+            Nom VARCHAR(255) NOT NULL,
+            Prenom VARCHAR(255) NOT NULL
+          );
 
 -- table Commande
-CREATE TABLE Commande (
-    CommandeId INT PRIMARY KEY,
-    ClientId INT NOT NULL,
-    RestaurantId INT NOT NULL,
-    Date DATETIME NOT NULL,
-    Prix INT NOT NULL,
-    LivraisonId INT NOT NULL,
-    Commentaire TEXT,
-    FOREIGN KEY (clientId) REFERENCES Client(ClientId),
-    FOREIGN KEY (LivraisonId) REFERENCES Livraison(LivraisonId),
-    FOREIGN KEY (RestaurantId) REFERENCES Restaurant(RestaurantId),
-);
+CREATE TABLE IF NOT EXISTS  Commande (
+            CommandeId INT PRIMARY KEY AUTO_INCREMENT,
+            ClientId INT NOT NULL,
+            RestaurantId INT NOT NULL,
+            Date DATETIME NOT NULL,
+            Prix DECIMAL(10,2) NOT NULL,
+            Commentaire TEXT,
+            FOREIGN KEY (clientId) REFERENCES Client(ClientId),
+            FOREIGN KEY (RestaurantId) REFERENCES Restaurant(RestaurantId)
+        )
 
 -- table Livreur
-CREATE TABLE Livraison (
-    LivraisonId INT PRIMARY KEY,
-    CommandeId INT NOT NULL,
-    AdresseId INT NOT NULL,
-    Effectue BOOLEAN NOT NULL,
-    ProblemeId INT,
-    FOREIGN KEY (AdresseId) REFERENCES Adresse(AdresseId),
-    FOREIGN KEY (ProblemeId) REFERENCES Probleme(ProblemeId)
-);
+CREATE TABLE IF NOT EXISTS  Livraison (
+            LivraisonId INT PRIMARY KEY AUTO_INCREMENT,
+            CommandeId INT NOT NULL,
+            AdresseId INT NOT NULL,
+            Effectue BOOLEAN NOT NULL,
+            ProblemeId INT,
+            FOREIGN KEY (AdresseId) REFERENCES Adresse(AdresseId),
+            FOREIGN KEY (ProblemeId) REFERENCES Probleme(ProblemeId)
+        );
 
 -- table Plat
-CREATE TABLE Plat (
-  PlatId INT PRIMARY KEY,
-  Nom VARCHAR(255) NOT NULL,
-  Description TEXT,
-  Prix DECIMAL(10,2) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS  Plat (
+            PlatId INT PRIMARY KEY AUTO_INCREMENT,
+            Nom VARCHAR(255) NOT NULL,
+            Description TEXT,
+            Prix DECIMAL(10,2) NOT NULL
+          );
 
 -- table de liaison Ligne_commande
-CREATE TABLE LienCommandePlat (
-  CommandeId INT NOT NULL,
-  PlatId INT NOT NULL,
-  Quantite INT NOT NULL,
-  PRIMARY KEY (CommandeId, PlatId),
-  FOREIGN KEY (CommandeId) REFERENCES Commande(CommandeId),
-  FOREIGN KEY (PlatId) REFERENCES Plat(PlatId)
+CREATE TABLE IF NOT EXISTS  LienCommandePlat (
+            CommandeId INT NOT NULL,
+            PlatId INT NOT NULL,
+            Quantite INT NOT NULL,
+            PRIMARY KEY (CommandeId, PlatId),
+            FOREIGN KEY (CommandeId) REFERENCES Commande(CommandeId),
+            FOREIGN KEY (PlatId) REFERENCES Plat(PlatId)
 );
 
 -- table Problème
-CREATE TABLE Probleme (
-    ProblemeId INT PRIMARY KEY,
-    LivraisonId INT NOT NULL,
-    Message TEXT,
-    FOREIGN KEY (LivraisonId) REFERENCES Livraison(LivraisonId),
-);
+CREATE TABLE IF NOT EXISTS  Probleme (
+            ProblemeId INT PRIMARY KEY AUTO_INCREMENT,
+            Message TEXT
+        );
